078acdb3bbf0888aa0656213f0a4c32d8148a3c2
Carsten Duch <carsten@hashicorp.com>
2023-03-13 10:04:48 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+
+provider "hcp" {
+  client_id     = var.hcp_client_id
+  client_secret = var.hcp_client_secret
+}
+
+
+resource "hcp_hvn" "main" {
+  hvn_id         = var.hvn_id
+  cloud_provider = "aws"
+  region         = var.region
+  cidr_block     = "172.25.32.0/20"
+}
+
+data "terraform_remote_state" "hvn" {
+  backend = "remote"
+
+  config = {
+    organization = var.organization
+    workspaces = {
+      name = var.hvnworkspace
+    }
+  }
+}
+
+
+resource "hcp_vault_cluster" "vault" {
+  cluster_id      = "demo-vault"
+  hvn_id          = data.terraform_remote_state.hvn.outputs.hvn_id
+  tier            = "dev"
+  public_endpoint = true
+}
+
+
+output "vault_public_endpoint" {
+  value = "${hcp_vault_cluster.vault.vault_public_endpoint_url}"
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "hcp_client_id" {
+  description = "The HCP Client ID"
+  type        = string
+  sensitive   = false
+}
+
+variable "hcp_client_secret" {
+  description = "The HCP Client Secret (sensitive)"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+
+variable "organization" {
+  type = string
+  default = "carstenduch"
+}
+
+variable "hvnworkspace" {
+  type = "string"
+  default = "HCP_HVN"
+}
